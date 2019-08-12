b326c08fec561d05b036419477facb8fee788736
Joseph Sunny <joseph@brew.com>
2019-08-12 10:54:00 +0530

    authentication

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,13 +50,28 @@ module.exports = {
   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
+    '@nuxtjs/auth',
+    '@nuxtjs/toast',
+    'bootstrap-vue/nuxt'
   ],
   /*
   ** Axios module configuration
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
+  baseURL: "http://192.168.8.141:5000/api/v1"
+  },
+
+  loading: {
+    name: 'chasing-dots',
+    color: '#ff5638',
+    background: 'white',
+    height: '4px'
+ },
+
+  toast: {
+    position: 'top-right',
+    duration: 2000
   },
   /*
   ** Build configuration
@@ -66,6 +81,34 @@ module.exports = {
     ** You can extend webpack config here
     */
     extend(config, ctx) {
+    }
+  },
+  
+  auth: {
+    strategies: {
+      local: {
+        endpoints: {
+          login: {url: '/user/login', method: 'post', propertyName:    'token' },
+          logout: false,
+          user: {url: '/user/user', method: 'get', propertyName: 'data'},
+        },
+        tokenRequired: true,
+        tokenType: 'Bearer'
+      },
+      facebook: {
+        client_id: 'your facebook app id',
+        userinfo_endpoint: 'https://graph.facebook.com/v2.12/me?fields=about,name,picture{url},email',
+        scope: ['public_profile', 'email']
+      },
+      google: {
+        client_id: 'your gcloud oauth app client id'
+      },
+    },
+    redirect: {
+      login: '/?login=1',
+      logout: '/',
+      user: '/profile',
+      callback:'/'
     }
   }
 }
